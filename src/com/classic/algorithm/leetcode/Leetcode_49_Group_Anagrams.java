bbe9d1cbc2f64a64e394a9d5cda5c9f3d0303e7b
hui <1919cc@gmail.com>
2019-01-15 16:02:04 +0800

    Leetcode_49_Group_Anagrams

---
+++ src/com/classic/algorithm/leetcode/Leetcode_49_Group_Anagrams.java
@@ -0,0 +1,30 @@
+package com.classic.algorithm.leetcode;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+
+public class Leetcode_49_Group_Anagrams {
+    public List<List<String>> groupAnagrams(String[] strs) {
+        Map<String, List<String>> map = new HashMap<>();
+        for (String ss : strs) {
+            char[] ssArray = ss.toCharArray();
+            Arrays.sort(ssArray);
+            String key = new String(ssArray);
+            
+            if (!map.containsKey(key)) {
+                List<String> res = new LinkedList<>();
+                res.add(ss);
+                map.put(key, res);
+            } else {
+                List<String> res = (List<String>)map.get(key);
+                res.add(ss);
+            }
+        }
+        List<List<String>> result = new LinkedList<>();
+        for (Map.Entry en : map.entrySet()) result.add((List<String>)en.getValue());
+        return result;
+    }
+}
