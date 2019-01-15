f4b0174be593f8fdb00bec193524f7c19d79e3a2
hui <1919cc@gmail.com>
2019-01-15 12:30:02 +0800

    Leetcode240Search_a_2D_Matrix_II

---
+++ src/com/classic/algorithm/leetcode/Leetcode240Search_a_2D_Matrix_II.java
@@ -0,0 +1,14 @@
+package com.classic.algorithm.leetcode;
+
+public class Leetcode240Search_a_2D_Matrix_II {
+    public boolean searchMatrix(int[][] matrix, int target) {
+        int row = matrix.length-1;
+        int col = 0;
+        while (row >= 0 && col < matrix[0].length) {
+            if (matrix[row][col] > target) row--;
+            else if (matrix[row][col] < target) col++;
+            else return true;
+        }
+        return false;
+    }
+}
